8d1e5ca5577a09bf1f308658b6a6d4e5135ecd44
Evgeny Tomsen <etomsen@Evgenys-MacBook-Pro.local>
2023-03-03 10:01:46 +0100

    refactor: remove subscriptions from product service

--- src/app/products/list/products-list.component.ts
+++ src/app/products/list/products-list.component.ts
@@ -1,27 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Product } from "../../shared/product";
 import { ProductsService } from "../shared/products.service";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
+import { distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators';
 
 @Component({
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent implements OnInit {
-  products$: Observable<Product[]>
+export class ProductsListComponent {
+  private readonly searchSubj = new Subject<string>();
+  public readonly products$: Observable<Product[]>
 
   query: string;
 
   constructor(private productsService: ProductsService) {
-    this.products$ = this.productsService.products$;
+    const search$ = this.searchSubj.asObservable().pipe(
+      map(s => s.toLowerCase()),
+      distinctUntilChanged(),
+      startWith('')
+    );
+    this.products$ = search$.pipe(
+      switchMap(filter => this.productsService.getFiltered$({filter}))
+    );
   }
 
   onSearch(query: string): void {
-    this.query = query;
-    this.productsService.get({filter: query});
+    this.searchSubj.next(query);
   }
 
-  ngOnInit(): void {
-    this.productsService.get();
-  }
 }
